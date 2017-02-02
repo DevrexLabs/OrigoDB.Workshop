004fbc893e26adc24581516715302c62840f5303
rofr <robert@devrex.se>
2017-02-02 20:45:03 +0100

    wip, migrating machine

---
+++ Modules/01_Introduction/Ex02/Todo.Core/Todo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+
+namespace Todo.Core
+{
+    [Serializable]
+    public class Todo
+    {
+        public readonly int Id;
+        public string Title { get; private set; }
+        public DateTime? Due { get; private set; }
+        public DateTime? Completed { get; private set; }
+
+        public Todo(int id)
+        {
+            Id = id;
+        }
+    }
+}
---
+++ Modules/01_Introduction/Ex02/Todo.Core/TodoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using OrigoDB.Core;
+
+namespace Todo.Core
+{
+    [Serializable]
+    public class TodoModel : Model
+    {
+        public SortedDictionary<int, Todo> Todos { get; private set; }
+
+        public TodoModel()
+        {
+            Todos = new SortedDictionary<int, Todo>();
+        }
+    }
+}
